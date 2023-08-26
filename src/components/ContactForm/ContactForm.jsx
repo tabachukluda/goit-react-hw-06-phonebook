dd87bf54636fb8090878846929b211db0cfed5bc
tabachukluda <121580942+tabachukluda@users.noreply.github.com>
2023-08-26 21:58:23 +0200

    pattern сchange

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -50,7 +50,7 @@ const ContactForm = ({ handleSubmit, contacts }) => {
                 className={css.formName}
                 type="text"
                 name="name"
-                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                 title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                 required
                 placeholder="Enter name"
@@ -62,7 +62,7 @@ const ContactForm = ({ handleSubmit, contacts }) => {
                 className={css.formNumber}
                 type="tel"
                 name="number"
-                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
                 title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                 required
                 placeholder="Enter phone number"
